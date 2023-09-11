060cdd1be2becc5630261d81acdc2db645fad73d
Yannis OURAGHI <yannis.ouraghi@viacesi.fr>
2023-09-11 16:07:00 +0200

    modification de l'interface

--- ProjetAnnuaire/MainWindow.xaml.cs
+++ ProjetAnnuaire/MainWindow.xaml.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Net.Http;
-using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,8 +15,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Newtonsoft.Json;
-
+using System.IO;
+using System.Text.Json.Serialization;
+using FontAwesome.WPF;
+using FontAwesome;
 
 namespace ProjetAnnuaire
 {
@@ -29,46 +31,104 @@ namespace ProjetAnnuaire
 
         public ObservableCollection<Employee> Employees { get; set; }
 
+        public Visibility AdminExpanderVisibility
+        {
+            get { return (Visibility)GetValue(AdminExpanderVisibilityProperty); }
+            set { SetValue(AdminExpanderVisibilityProperty, value); }
+        }
+
+        public static readonly DependencyProperty AdminExpanderVisibilityProperty =
+            DependencyProperty.Register("AdminExpanderVisibility", typeof(Visibility), typeof(MainWindow), new PropertyMetadata(Visibility.Collapsed));
+
+
+
         public MainWindow()
         {
             InitializeComponent();
             Employees = new ObservableCollection<Employee>();
             _httpClient = new HttpClient();
+            CommandBindings.Add(new CommandBinding(OpenAdminCommand, OpenAdminCommand_Executed, OpenAdminCommand_CanExecute));
+            InputBindings.Add(new KeyBinding(OpenAdminCommand, new KeyGesture(Key.A, ModifierKeys.Control)));
 
-            LoadEmployees();
+            //LoadEmployees();
             DataContext = this;
         }
 
-        private async Task<IEnumerable<Employee>> GetAllEmployees()
+        private void Deconnexion_Click(object sender, RoutedEventArgs e)
         {
-            HttpResponseMessage response = await _httpClient.GetAsync("https://localhost:7053/api/Employee");
+            AdminExpanderVisibility = Visibility.Collapsed;
+        }
 
-            if (response.IsSuccessStatusCode)
+        private void RechercherButton_Click(object sender, RoutedEventArgs e)
+        {
+            contentFrame.Navigate(new Uri("RechercherPage.xaml", UriKind.Relative));
+        }
+
+
+        //private async Task<IEnumerable<Employee>> GetAllEmployees()
+        //{
+        //    HttpResponseMessage response = await _httpClient.GetAsync("https://localhost:7053/api/Employee");
+
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        using (var contentStream = await response.Content.ReadAsStreamAsync())
+        //        {
+        //            var employees = await System.Text.Json.JsonSerializer.DeserializeAsync<IEnumerable<Employee>>(contentStream);
+        //            return employees;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        return new List<Employee>();
+        //    }
+        //}
+        public static RoutedCommand OpenAdminCommand = new RoutedCommand();
+
+        private void OpenAdminCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenAdminWindow();
+        }
+
+        private void OpenAdminCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            // Vous pouvez ajouter ici des conditions pour activer ou désactiver la commande si nécessaire.
+            e.CanExecute = true;
+        }
+
+        private void OpenAdminWindow()
+        {
+            PasswordDialog passwordWindow = new PasswordDialog();
+            bool? result = passwordWindow.ShowDialog();
+
+            if (result == true && passwordWindow.EnteredPassword == "admin")
             {
-                var contentStream = await response.Content.ReadAsStreamAsync();
-                var employees = await System.Text.Json.JsonSerializer.DeserializeAsync<IEnumerable<Employee>>(contentStream);
-                return employees;
+                // Le mot de passe est correct, affichez le AdminExpander
+                AdminExpanderVisibility = Visibility.Visible;
             }
             else
             {
-                return new List<Employee>();
+                MessageBox.Show("Mot de passe incorrect");
             }
         }
 
 
-        private async void LoadEmployees()
-        {
-            IEnumerable<Employee> employees = await GetAllEmployees();
-            foreach (Employee employee in employees)
-            {
-                Employees.Add(employee);
-            }
-        }
 
 
-        private void Search_Click(object sender, RoutedEventArgs e)
-        {
-            LoadEmployees();
-        }
+        // Appeler cette méthode pour ouvrir la fenêtre admin
+
+
+
+
+
+        //private async void LoadEmployees()
+        //{
+        //    IEnumerable<Employee> employees = await GetAllEmployees();
+        //    foreach (Employee employee in employees)
+        //    {
+        //        MessageBox.Show(employee.FirstName);
+        //        Employees.Add(employee);
+        //    }
+        //}
+
     }
 }
---
+++ ProjetAnnuaire/RechercherPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using APIAnnuaire.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjetAnnuaire
+{
+    /// <summary>
+    /// Logique d'interaction pour RechercherPage.xaml
+    /// </summary>
+    public partial class RechercherPage : Page
+    {
+        public RechercherPage()
+        {
+            InitializeComponent();
+        }
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            var SearchSite = txtSearchBySite.Text;
+            var SearchName = txtSearchByName.Text;
+            var SearchService = txtSearchByService.Text;
+
+            //if (SearchSite != "")
+            //{
+            //    var result = Employees.Where(x => x.Site == SearchSite);
+            //    Employees.Clear();
+            //    foreach (Employee employee in result)
+            //    {
+            //        Employees.Add(employee);
+            //    }
+            //}
+            //if (SearchName != "")
+            //{
+            //    var result = Employees.Where(x => x.LastName == SearchName);
+            //    Employees.Clear();
+            //    foreach (Employee employee in result)
+            //    {
+            //        Employees.Add(employee);
+            //    }
+            //}
+            //if (SearchService != "")
+            //{
+            //    var result = Employees.Where(x => x.Service == SearchService);
+            //    Employees.Clear();
+            //    foreach (Employee employee in result)
+            //    {
+            //        Employees.Add(employee);
+            //    }
+            //}
+
+        }
+    }
+}
