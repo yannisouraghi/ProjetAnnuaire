4756a3d3eb4ddb4777e1565c4aad54aec1fa186e
Ouraghi Yannis <85176113+yannisouraghi@users.noreply.github.com>
2023-10-30 16:11:48 +0100

    Add files via upload

---
+++ Model/Employees.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjetAnnuaire.Model
+{
+    public class Employees
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int EmployeeId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Department { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? MobilePhone { get; set; }
+        public string? JobTitle { get; set; }
+        public string? JobDescription { get; set; }
+
+        // Clé étrangère pour Site
+        public int? SiteId { get; set; }
+        //public Sites? Sites { get; set; }
+        public int ServiceId { get; set; }
+
+        public string? City { get; set; }
+    }
+}
---
+++ Model/Services.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetAnnuaire.Model
+{
+    public class Services
+    {
+        [Key]
+        public int ServiceId { get; set; }
+        public string? Service { get; set; }
+    }
+}
